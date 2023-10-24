daab64db5cf8c1ea4adc76cf480ae6fca4a46826
Daniel Mårtensson <daniel.martensson89@outlook.com>
2023-10-24 22:12:16 +0000

    fix devcontainer

--- .devcontainer/post-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run docker-compose up in the background
-docker-compose up -d
-# Pull another repository
-git clone https://github.com/x-danma/codespaces-test-backend.git
-# Change directory to the repo folder
-cd codespaces-test-backend
-# Run the dotnet command
-dotnet
